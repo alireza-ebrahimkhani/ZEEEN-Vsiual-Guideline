6ca411cee7123c3fa093dff45abd40a3211ffa91
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-04-08 19:00:31 +0430

    a

--- js/js.js
+++ js/js.js
@@ -5,27 +5,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let mainNav__div = document.querySelector(".header__items");
     let logoDynamic__svg = document.querySelector(".header__logoSvgDynamic");
     let body__body = document.querySelector("body");
-    
-    hamico_div.addEventListener("click" , togglemenu);
+
+    hamico_div.addEventListener("click", togglemenu);
     let showMenu = false;
-    function togglemenu(){
-        if (!showMenu){
+    function togglemenu() {
+        if (!showMenu) {
             line_div.classList.add('header__hamline--open');
             mainNav__div.classList.add('header__items--open');
             logoDynamic__svg.classList.add('header__logoSvgDynamic--inline');
             body__body.style.overflow = "hidden";
             showMenu = true;
-        }else {
+        } else {
             line_div.classList.remove('header__hamline--open');
             mainNav__div.classList.remove('header__items--open');
             logoDynamic__svg.classList.remove('header__logoSvgDynamic--inline');
             body__body.style.overflow = "auto";
             showMenu = false;
         }
-            
+
     }
-    window.addEventListener("resize" , function(){ 
-        if(showMenu && window.outerWidth >= 900){
+    window.addEventListener("resize", function () {
+        if (showMenu && window.outerWidth >= 900) {
             line_div.classList.remove('header__hamline--open');
             mainNav__div.classList.remove('header__items--open');
             logoDynamic__svg.classList.remove('header__logoSvgDynamic--inline');
@@ -34,21 +34,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
     let heading_h2 = document.querySelectorAll('.node-name--H2');
-            heading_h2.forEach(heading => {
-                heading.addEventListener('click' , closeMenu);
-                function closeMenu(){
-                line_div.classList.remove('header__hamline--open');
-                mainNav__div.classList.remove('header__items--open');
-                body__body.style.overflow = "auto";
-                showMenu = false;
+    heading_h2.forEach(heading => {
+        heading.addEventListener('click', closeMenu);
+        function closeMenu() {
+            line_div.classList.remove('header__hamline--open');
+            mainNav__div.classList.remove('header__items--open');
+            body__body.style.overflow = "auto";
+            showMenu = false;
+        }
+
+    });
+
+    let colorSection_section = document.querySelectorAll(".panel");
+    window.addEventListener('scroll', function () {
+        let nowScroll = window.pageYOffset;
+        for(let i = 0; i < colorSection_section.length ; i++){
+            let colorStart= colorSection_section[i].offsetTop;
+            let colorEnd = colorSection_section[i].scrollHeight + colorStart;
+            let color = colorSection_section[i].dataset.color;
+
+                if (nowScroll > colorStart && nowScroll < colorEnd){
+                    body__body.className = "";;
+                    body__body.classList.add(color);
+                }else{
+                    continue;
                 }
-                
-            });
-           
-  });
+            
+            /*if (nowScroll > colorStart && nowScroll < colorEnd){
+                console.log("hi")
+                body__body.classList.add(color);
+            }if(nowScroll > colorEnd || nowScroll < colorStart){
+                body__body.classList.remove(color);
+            }*/
+        }
+    });
+});
 
 //translate switch
-function toggleSwitch(){
+function toggleSwitch() {
     var transSwitch = document.querySelector(".header__transLine").classList.toggle('header__transLine--active');;
     var transPin = document.querySelector(".header__transPin").classList.toggle('header__transPin--active');
     var textFa_p = document.querySelectorAll(".fa");
@@ -74,9 +97,8 @@ window.addEventListener('scroll' , function(){
         mainNav_div.classList.remove('main__nav--up');
         hamico_div.classList.remove('header__hamico--hidden');
     }
-    
+
     lastScroll = nowScroll;
     }
-    
 });
 */
