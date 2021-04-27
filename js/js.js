91a8a41265b3a70112271691633c2bab70c6c14a
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-04-27 17:08:37 +0430

    a

--- js/js.js
+++ js/js.js
@@ -45,7 +45,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     let colorSection_section = document.querySelectorAll(".panel");
-    console.log(colorSection_section);
     window.addEventListener('scroll', function () {
         let nowScroll = window.pageYOffset;
         for(let i = 0; i < colorSection_section.length ; i++){
@@ -72,26 +71,3 @@ function toggleSwitch() {
         textFa_p.classList.toggle('fa--show');
     });
 }
-//hide header
-/*var lastScroll = 0, delta= 200;
-window.addEventListener('scroll' , function(){
-    var mainNav__div = document.querySelector(".main__nav");
-    var nowScroll = window.pageYOffset;
-    if (Math.abs(lastScroll - nowScroll) > delta){
-        var header_header = document.querySelector('header');
-        var mainNav_div = document.querySelector('.main__nav');
-        var hamico_div = document.querySelector('.header__hamico');
-    if (nowScroll > lastScroll &&  lastScroll != 0 && mainNav__div.classList.contains("main__nav--open")==false){
-        header_header.classList.add('header--hidden');
-        mainNav_div.classList.add('main__nav--up');
-        hamico_div.classList.add('header__hamico--hidden');
-    } else if(nowScroll < lastScroll) {
-        header_header.classList.remove('header--hidden');
-        mainNav_div.classList.remove('main__nav--up');
-        hamico_div.classList.remove('header__hamico--hidden');
-    }
-
-    lastScroll = nowScroll;
-    }
-});
-*/
