832628858c1b14a6e380c7b9829168113f7f2e3e
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-07-06 22:57:21 +0430

    a

--- js/imgCmp.js
+++ js/imgCmp.js
@@ -1,25 +1,28 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
   var imgComparison_div = document.querySelectorAll(".imgComparison");
-  
-    imgComparison_div.forEach(imgComparison_div => {
-      var resizableImg_img = imgComparison_div.querySelector('.imgComparison__resizableImg');
-      var fixedImg_img = imgComparison_div.querySelector('.imgComparison__fixedImg');
-      var mainWidth = imgComparison_div.getBoundingClientRect().width;
+
+  imgComparison_div.forEach(el => {
+    let resizableImg_img = el.querySelector('.imgComparison__resizableImg');
+    let fixedImg_img = el.querySelector('.imgComparison__fixedImg');
+    let mainWidth = el.getBoundingClientRect().width;
+    resizableImg_img.style.width = mainWidth + "px";
+    fixedImg_img.style.width = mainWidth + "px";
+
+
+    let slider = el.querySelector('.imgComparison__silder');
+    slider.addEventListener('input', slied => {
+      var resizableFrame_div = el.querySelector('.imgComparison__resizableFrame');
+      resizableFrame_div.style.width = slied.target.value + '%';
+    });
+
+    window.addEventListener('resize', function () {
+      let mainWidth = el.getBoundingClientRect().width;
       resizableImg_img.style.width = mainWidth + "px";
       fixedImg_img.style.width = mainWidth + "px";
-
-        window.addEventListener('resize' , compare);
-        function compare(){
-          var resizableImg_img = imgComparison_div.querySelector('.imgComparison__resizableImg');
-          var fixedImg_img = imgComparison_div.querySelector('.imgComparison__fixedImg');
-          var mainWidth = imgComparison_div.getBoundingClientRect().width;
-          resizableImg_img.style.width = mainWidth + "px";
-          fixedImg_img.style.width = mainWidth + "px";
-        };
-        var slider = imgComparison_div.querySelector('.imgComparison__silder');
-        slider.addEventListener('input', slied => {
-          var resizableFrame_div = imgComparison_div.querySelector('.imgComparison__resizableFrame');
-          resizableFrame_div.style.width = slied.target.value + '%';
-        });
     });
+  });
+
+
 });
